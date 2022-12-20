4a7ec7cb8997c2a7a88c6327a7d11a3924aba70c
stylebytemi <teemaybelle@gmail.com>
2022-12-20 15:01:47 -0800

    add correction

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 19 14:21:54 2020
-@author: Robinson Montes
-"""
+# 0-square.py
+"""Define a class Square."""
 class Square:
-    """Empty class Square that defines a square
-    Attributes:
-        empty
-    """
+    """Represent a square."""
     pass
