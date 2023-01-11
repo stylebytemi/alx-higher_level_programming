840476c1909c61e18760e2e753ab916ac9f2c73b
stylebytemi <teemaybelle@gmail.com>
2023-01-10 19:02:10 -0800

    addk

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
+
     try:
         for line in sys.stdin:
             if count == 10:
@@ -33,11 +34,14 @@ if __name__ == "__main__":
                 count = 1
             else:
                 count += 1
+
             line = line.split()
+
             try:
                 size += int(line[-1])
             except (IndexError, ValueError):
                 pass
+
             try:
                 if line[-2] in valid_codes:
                     if status_codes.get(line[-2], -1) == -1:
@@ -46,7 +50,9 @@ if __name__ == "__main__":
                         status_codes[line[-2]] += 1
             except IndexError:
                 pass
+
         print_stats(size, status_codes)
+
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
