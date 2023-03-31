27be65460fa934a9ada822f3112e5ea63065c748
stylebytemi <teemaybelle@gmail.com>
2023-03-31 14:52:59 -0700

    make edit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     with urlopen(url) as response:
         bytes_content = response.read()
         content = bytes_content.decode('utf-8')
