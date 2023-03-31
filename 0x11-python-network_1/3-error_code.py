7602c3bc8a9d208ef80f32c37bfa1acddad7e47a
stylebytemi <teemaybelle@gmail.com>
2023-03-31 15:36:07 -0700

    edit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
+"""
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
