d34f5928daf638366fc17dbf45b730eb5096ed7a
Temiloluwa Adewale <98469754+stylebytemi@users.noreply.github.com>
2023-07-27 00:08:34 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,31 @@
 #!/usr/bin/node
 const request = require('request');
+
 request(process.argv[2], function (error, response, body) {
-  if (!error) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error: Unexpected status code:', response.statusCode);
+    return;
+  }
+
+  try {
     const todos = JSON.parse(body);
     const completed = {};
     todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+      if (todo.completed) {
+        if (completed[todo.userId] === undefined) {
+          completed[todo.userId] = 1;
+        } else {
+          completed[todo.userId] += 1;
+        }
       }
     });
     console.log(completed);
+  } catch (parseError) {
+    console.error('Error: Failed to parse JSON response:', parseError);
   }
 });
