5e3c4f7ad4cd44f307b5a352126bc981ad59fce0
Temiloluwa Adewale <98469754+stylebytemi@users.noreply.github.com>
2023-07-27 00:10:20 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,31 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
+const url = process.argv[2];
+let data;
+const dictionary = {};
 
-request(process.argv[2], function (error, response, body) {
+request(url, function (error, response, body) {
   if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error: Unexpected status code:', response.statusCode);
-    return;
-  }
-
-  try {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed) {
-        if (completed[todo.userId] === undefined) {
-          completed[todo.userId] = 1;
-        } else {
-          completed[todo.userId] += 1;
+    console.error(error);
+  } else {
+    data = JSON.parse(body);
+    data.forEach(function (result) {
+      if (result.completed === true) {
+        const userid = result.userId;
+        if (!(userid in dictionary)) {
+          dictionary[userid] = 0;
         }
+        dictionary[userid] += 1;
       }
     });
-    console.log(completed);
-  } catch (parseError) {
-    console.error('Error: Failed to parse JSON response:', parseError);
+    console.log(dictionary);
   }
 });
