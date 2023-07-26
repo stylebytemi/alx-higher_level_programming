e659f779a2a8f2f99178f9d256c6798abb743d0a
Temiloluwa Adewale <98469754+stylebytemi@users.noreply.github.com>
2023-07-26 23:36:46 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -10,21 +10,27 @@ function MakeRequest(url) {
       if (!error && response.statusCode === 200) {
         resolve(JSON.parse(body));
       } else {
-        reject(error);
+        reject(error || new Error('Request failed'));
       }
     });
   });
 }
 
 async function main() {
+  if (argv.length < 3) {
+    console.error('Please provide the Movie ID as the first argument.');
+    process.exit(1);
+  }
+
   try {
-    const movie = await MakeRequest(BaseUrl + argv[2]);
+    const movieId = argv[2];
+    const movie = await MakeRequest(BaseUrl + movieId);
     const characters = movie.characters;
 
-    for (const element of characters) {
-      const character = await MakeRequest(element);
-      const CharacterName = character.name;
-      console.log(CharacterName);
+    for (const characterUrl of characters) {
+      const character = await MakeRequest(characterUrl);
+      const characterName = character.name;
+      console.log(characterName);
     }
   } catch (error) {
     console.error('Error:', error.message);
